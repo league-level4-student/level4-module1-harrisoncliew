c65a5c96beb755384131c96ac3846692eebe2565
harrisoncliew <41922141+harrisoncliew@users.noreply.github.com>
2020-07-15 11:28:40 -0700

    ArrayList (IP)

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,16 +3,23 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
-	
+	T[] array;
 	public ArrayList() {
+		array  = (T[]) new Object[0];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+		return array[loc];
 	}
 	
 	public void add(T val) {
+		T[] array2 = (T[]) new Object[array.length+1];
+		array2[array.length] = val;
+		for (int i = 0; i < array.length; i++) {
+			array2[i] = array[i];
+		}
+		array = array2;
+		
 		
 	}
 	
